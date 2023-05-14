fc7029cc39dd453f95be81c6cfb105f17d2a17d7
barakshamir <barak20000@gmail.com>
2023-05-14 22:14:02 +0300

    commit

---
+++ src/test/java/com/team/test/Testing.java
@@ -0,0 +1,22 @@
+package com.team.test;
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class Testing {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Testing.class.getName());
+
+    @Test
+    public void fff() throws InterruptedException {
+        LOGGER.info("selenium test");
+        System.setProperty("webdriver.gecko.driver","/Users/barakshamir/Documents/temp/geckodriver"); // Setting system properties of FirefoxDriver
+        WebDriver driver = new FirefoxDriver(); //Creating an object of FirefoxDriver
+        driver.get("https://www.google.com/");
+        driver.close();
+    }
+}
