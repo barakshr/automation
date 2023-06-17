0ecc90165b94965055cc388757743c697a09b3e6
barakshamir <barak20000@gmail.com>
2023-06-17 20:16:58 +0300

    commit

--- src/main/java/com/team/framwork/selenium/WebDriverFactory.java
+++ src/main/java/com/team/framwork/selenium/WebDriverFactory.java
@@ -1,8 +1,12 @@
 package com.team.framwork.selenium;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.devtools.DevTools;
+import org.openqa.selenium.devtools.v85.log.Log;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.time.Duration;
@@ -10,6 +14,7 @@ import java.time.Duration;
 
 public class WebDriverFactory {
 
+    private final static Logger logger = LogManager.getLogger(WebDriverFactory.class);
 
     public static void openNewWebDriver(BrowserType browserType) throws Exception {
         String path = System.getProperty("user.dir") + "/src/main/resources/drivers/";
@@ -27,7 +32,9 @@ public class WebDriverFactory {
                    webDriver.get("www.cnn.com");
                 */
                 System.setProperty("webdriver.chrome.driver", path + "chromedriver");
-                webDriver = new ChromeDriver();
+                ChromeDriver chromeDriver = new ChromeDriver();
+                cdpLogs(chromeDriver.getDevTools());
+                webDriver = chromeDriver;
                 break;
 
             case IE:
@@ -37,5 +44,18 @@ public class WebDriverFactory {
         webDriver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(3));
         webDriver.manage().timeouts().scriptTimeout(Duration.ofSeconds(3));
         DriverPool.getInstance().setDriver(Thread.currentThread().getId(), webDriver);
+    }
+
+    //devTools only available for chrome and edge
+    private static void cdpLogs(DevTools devTools) {
+        devTools.createSession();
+        devTools.send(Log.enable());
+        devTools.addListener(Log.entryAdded(), logEntry -> {
+            logger.info("--------Browser Console Log Start-------");
+            logger.info("Level: " + logEntry.getLevel());
+            logger.info("Text: " + logEntry.getText());
+            logger.info("Broken URL: " + logEntry.getUrl());
+            logger.info("--------Browser Console Log End---------");
+        });
     }
 }
