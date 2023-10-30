af00514901cc86e48f7d329b60ef36ea5410a991
barakshamir <barak20000@gmail.com>
2023-10-30 16:07:47 +0200

    -

--- src/main/java/com/team/framwork/selenium/BrowserType.java
+++
@@ -1,7 +0,0 @@
-package com.team.framwork.selenium;
-
-public enum BrowserType {
-    Firefox,
-    IE,
-    Chrome
-}
--- src/main/java/com/team/selenium_pages/pages/herokuapp/ViewProfilePage.java
+++
@@ -1,7 +0,0 @@
-package com.team.selenium_pages.pages.herokuapp;
-
-import com.team.selenium_pages.pages.BasePage;
-
-public class ViewProfilePage  extends BasePage {
-
-}
