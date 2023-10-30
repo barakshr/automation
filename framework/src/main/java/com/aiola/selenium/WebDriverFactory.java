3b1ac258306eaf8f51df75853baed9aa28740fe3
barakshamir <barak20000@gmail.com>
2023-10-30 19:49:52 +0200

    -

--- framework/src/main/java/com/aiola/selenium/WebDriverFactory.java
+++ framework/src/main/java/com/aiola/selenium/WebDriverFactory.java
@@ -6,14 +6,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.firefox.FirefoxProfile;
-import org.openqa.selenium.firefox.ProfilesIni;
-
-import java.util.HashMap;
-import java.util.Map;
 
 
 public class WebDriverFactory {
